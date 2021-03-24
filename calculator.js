1d532724e407b95bf749f603d94140471dea30be
Thomas <thomas.laine25+1@gmail.com>
2021-03-24 15:53:34 +0100

    added event handling and display results

--- calculator.js
+++ calculator.js
@@ -1,3 +1,5 @@
+// MATH FUNC
+
 const add = (nb1, nb2) => {
   return nb1 + nb2;
 };
@@ -28,3 +30,46 @@ const operate = (operator, nb1, nb2) => {
     return divide(nb1, nb2);
   }
 };
+
+// EVENT HANDLING
+let nb1 = "";
+let nb2 = "";
+let tmp = "";
+let operator = "";
+let res = 0;
+
+let buttonNb = document.getElementsByClassName("nb");
+const buttonClear = document.getElementById("clear");
+const buttonOperator = document.getElementsByClassName("operator");
+const buttonEqual = document.getElementById("equal");
+
+document.getElementById("displayNb").textContent = 0;
+
+for (let i = 0; i < buttonNb.length; i++) {
+  buttonNb[i].addEventListener("click", (e) => {
+    tmp += e.target.value;
+    document.getElementById("displayNb").innerHTML = tmp;
+  });
+}
+
+for (let i = 0; i < buttonOperator.length; i++) {
+  buttonOperator[i].addEventListener("click", (e) => {
+    operator = e.target.value;
+    nb1 = parseInt(tmp);
+    tmp = "";
+  });
+}
+
+buttonEqual.addEventListener("click", () => {
+  nb2 = parseInt(tmp);
+  res = operate(operator, nb1, nb2);
+  document.getElementById("displayNb").innerHTML = res;
+  tmp = res;
+});
+
+buttonClear.addEventListener("click", () => {
+  document.getElementById("displayNb").textContent = 0;
+  nb1 = 0;
+  nb2 = 0;
+  tmp = "";
+});
